b2d1ea8f9f10601d1d2df7d0a81c7a9844d9a824
wunyinwilliam <wunyinwilliam@gmail.com>
2020-11-04 18:02:24 +0800

    Pages Added

---
+++ Hiking/Hiking/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewController.swift
+//  Hiking
+//
+//  Created by Will Lam on 4/11/2020.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if firstLaunch == true {
+            firstLaunch = false
+            self.selectedIndex = 2 // 3rd tab
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
