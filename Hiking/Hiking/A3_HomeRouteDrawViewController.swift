e0474d8b0f6078dbbe48b4484b492fcc360d8373
wunyinwilliam <wunyinwilliam@gmail.com>
2020-11-05 12:34:36 +0800

    Pages added. Pod added

---
+++ Hiking/Hiking/A3_HomeRouteDrawViewController.swift
@@ -0,0 +1,29 @@
+//
+//  A3_HomeRouteDrawViewController.swift
+//  Hiking
+//
+//  Created by Will Lam on 5/11/2020.
+//
+
+import UIKit
+
+class A3_HomeRouteDrawViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
