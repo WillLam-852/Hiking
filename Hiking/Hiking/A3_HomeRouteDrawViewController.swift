87a7c6edc9fe12270feb0bbb4de625b3c300ead9
wunyinwilliam <wunyinwilliam@gmail.com>
2020-11-15 17:00:59 +0800

    AnnotationView Added

--- Hiking/Hiking/A3_HomeRouteDrawViewController.swift
+++ Hiking/Hiking/A3_HomeRouteDrawViewController.swift
@@ -10,32 +10,34 @@ import CoreLocation
 import MapKit
 import Contacts
 
+// Protocol for dropping the pin after searching in serach bar
 protocol HandleMapSearch {
     func dropPinZoomIn(placemark:MKPlacemark)
 }
 
-class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegate {
+class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
-    var resultSearchController: UISearchController? = nil
-    var selectedPin: MKPlacemark? = nil
-
     @IBOutlet weak var mapView: MKMapView!
     
     var currentPlacemark: CLPlacemark?
-    var currentPostalAddress: CNPostalAddress?
-    
+    var selectedPin: MKPlacemark? = nil
     var locationManager: CLLocationManager = CLLocationManager()
+    var resultSearchController: UISearchController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         mapView.showsUserLocation = true
-        locationManager.requestWhenInUseAuthorization()
+        mapView.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.delegate = self
-        locationManager.requestLocation()
+        if locationManager.authorizationStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        } else {
+            locationManager.requestLocation()
+        }
         
         // Insert the location search table during inputting in the search bar
         let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "A3_LocationSeatchTableID") as! A3_LocationSearchTableViewController
@@ -44,6 +46,7 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
         locationSearchTable.mapView = mapView
         locationSearchTable.handleMapSearchDelegate = self
         
+        // Insert the search bar in the navigation bar
         let searchBar = resultSearchController!.searchBar
         searchBar.sizeToFit()
         searchBar.placeholder = "Search for places"
@@ -52,6 +55,42 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
         navigationItem.titleView = resultSearchController?.searchBar
         resultSearchController?.hidesNavigationBarDuringPresentation = false
         definesPresentationContext = true
+        
+        // Insert the Long Press Gesture Recognizer in the map
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(self.addAnnotation(gestureRecognizer:)))
+        longPress.minimumPressDuration = 0.7
+        mapView.addGestureRecognizer(longPress)
+    }
+    
+    @objc func addAnnotation(gestureRecognizer:UIGestureRecognizer){
+        if gestureRecognizer.state == UIGestureRecognizer.State.began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: newCoordinates.latitude, longitude: newCoordinates.longitude), completionHandler: {(placemarks, error) -> Void in
+                if error != nil {
+                    print("Reverse geocoder failed with error" + error!.localizedDescription)
+                    return
+                }
+
+                if placemarks!.count > 0 {
+                    let pm = placemarks![0]
+
+                    // not all places have thoroughfare & subThoroughfare so validate those values
+                    annotation.title = pm.thoroughfare ?? "" + (pm.subThoroughfare ?? "")
+                    annotation.subtitle = pm.subLocality
+                    self.mapView.addAnnotation(annotation)
+                    print(pm)
+                } else {
+                    annotation.title = "Unknown Place"
+                    self.mapView.addAnnotation(annotation)
+                    print("Problem with the data received from geocoder")
+                }
+//            places.append(["name":annotation.title,"latitude":"\(newCoordinates.latitude)","longitude":"\(newCoordinates.longitude)"])
+            })
+        }
     }
     
 
@@ -66,48 +105,18 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
     */
     
     
-    // MARK: - Actions
-
-//    if let address = place_textField.text {
-//        CLGeocoder().geocodeAddressString(address) {
-//            (placemarks, error) in
-//            if error != nil {
-//                print("Geo failed with error: \(error!.localizedDescription)")
-//            } else if let marks = placemarks, marks.count > 0 {
-//                let placemark = marks[0]
-//                if let location = placemark.location {
-//                    CLGeocoder().reverseGeocodeLocation(location) {
-//                        (p, e) in
-//                        if e != nil {
-//                            print("Reverse geo failed with error: \(error!.localizedDescription)")
-//                        } else if let m = p, m.count > 0 {
-//                            let pm = m[0] as CLPlacemark
-//                            self.currentPlacemark = pm
-//                            if let add = pm.postalAddress {
-//                                self.currentPostalAddress = add
-//                            }
-//                        }
-//                    }
-//                }
-//
-//            }
-//        }
-//
-//        if let pm = currentPlacemark, let loc = pm.location {
-//            let searchCenter = CLLocationCoordinate2D(latitude: loc.coordinate.latitude, longitude: loc.coordinate.longitude)
-//            let searchRegion = MKCoordinateRegion(center: searchCenter, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-//            mapView.setRegion(searchRegion, animated: true)
-//
-//            let pin = MKPlacemark(coordinate: searchCenter)
-//            mapView.addAnnotation(pin)
-//        }
-//    }
-    
     
     // MARK: - CLLocationManagerDelegate
     
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard manager.authorizationStatus == .authorizedWhenInUse else {
+            return
+        }
+        manager.requestLocation()
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation:CLLocation = locations[0] as CLLocation
+        let userLocation: CLLocation = locations[0] as CLLocation
         let userRegion = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         mapView.setRegion(userRegion, animated: true)
         
@@ -119,6 +128,35 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error - locationManager: \(error.localizedDescription)")
+    }
+    
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "myMarker"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -10, y: 5)
+            view.leftCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            view.leftCalloutAccessoryView?.largeContentTitle = "selectLocation"
+            view.rightCalloutAccessoryView = UIButton(type: .close)
+            view.rightCalloutAccessoryView?.largeContentTitle = "diselectLocation"
+        }
+        return view
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control.largeContentTitle == "selectLocation" {
+            print("selectLocation")
+        } else if control.largeContentTitle == "diselectLocation" {
+            print("diselectLocation")
+        }
     }
 }
 
