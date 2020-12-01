6461cd596327b0d8c1e5f0dcdaf05a7de878acdc
wunyinwilliam <wunyinwilliam@gmail.com>
2020-12-01 21:27:09 +0800

    Add Annotations Checkboxes

--- Hiking/Hiking/A3_HomeRouteDrawViewController.swift
+++ Hiking/Hiking/A3_HomeRouteDrawViewController.swift
@@ -20,6 +20,17 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var checkbox_toilet: UIButton!
+    @IBOutlet weak var checkbox_campsite: UIButton!
+    @IBOutlet weak var checkbox_viewingpoint: UIButton!
+    @IBOutlet weak var checkbox_busstop: UIButton!
+    @IBOutlet weak var checkbox_pier: UIButton!
+    @IBOutlet weak var checkbox_beach: UIButton!
+    @IBOutlet weak var checkbox_sittingoutarea: UIButton!
+    @IBOutlet weak var checkbox_pavilion: UIButton!
+    @IBOutlet weak var checkbox_sunrise: UIButton!
+    @IBOutlet weak var checkbox_stargazing: UIButton!
+    
     var currentPlacemark: CLPlacemark?
     var selectedPin: MKPlacemark? = nil
     var selectedPinCoordinate: CLLocationCoordinate2D? = nil
@@ -29,6 +40,7 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
     var annotation_EndPoint: MKPointAnnotation? = nil
     var annotation_MidwayPoint: [(MKPointAnnotation, Int?)] = []
     var currentRoute: Route? = nil
+    var checkboxsValue = [true, true, true, true, true, true, true, true, true, true]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +48,10 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
         doneButton.isEnabled = false
         title = "HIKE"
         navigationController?.title = "Hike"
+        
+        for checkbox in [checkbox_toilet, checkbox_campsite, checkbox_viewingpoint, checkbox_busstop, checkbox_pier, checkbox_beach, checkbox_sittingoutarea, checkbox_pavilion, checkbox_sunrise, checkbox_stargazing] {
+            checkbox?.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+        }
         
         // Do any additional setup after loading the view.
         mapView.showsUserLocation = true
@@ -168,6 +184,26 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
                 view.glyphImage = UIImage(named: "Toilet")
             } else if view.annotation?.title == "Campsite" {
                 view.glyphImage = UIImage(named: "Campsite")
+            } else if view.annotation?.title == "ViewingPoint" {
+                view.glyphImage = UIImage(systemName: "camera.fill")
+            } else if view.annotation?.title == "BusStop" {
+                view.glyphImage = UIImage(systemName: "bus.doubledecker")
+            } else if view.annotation?.title == "Pier" {
+                view.glyphImage = UIImage(named: "Pier")
+            } else if view.annotation?.title == "Beach" {
+                view.glyphImage = UIImage(named: "Beach")
+            } else if view.annotation?.title == "SittingOutArea" {
+                view.glyphImage = UIImage(named: "SittingOutArea")
+            } else if view.annotation?.title == "Pavilion" {
+                view.glyphImage = UIImage(named: "Pavilion")
+            } else if view.annotation?.title == "Sunrise" {
+                view.glyphImage = UIImage(systemName: "sunrise.fill")
+            } else if view.annotation?.title == "Sunset" {
+                view.glyphImage = UIImage(systemName: "sunset.fill")
+            } else if view.annotation?.title == "Stargazing" {
+                view.glyphImage = UIImage(systemName: "moon.stars.fill")
+            } else if view.annotation?.title == "SelfDefined" {
+                view.glyphImage = UIImage(systemName: "mappin.and.ellipse")
             }
         }
         return view
@@ -353,6 +389,296 @@ class A3_HomeRouteDrawViewController: UIViewController, CLLocationManagerDelegat
             textField.placeholder = "Route Name"
         }
         self.present(alert, animated: true)
+    }
+    
+    @IBAction func pressedCheckbox_toilet(_ sender: UIButton) {
+        let location = "Toilet"
+        if !checkboxsValue[0] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[0] = !checkboxsValue[0]
+    }
+    
+    @IBAction func pressedCheckbox_campsite(_ sender: UIButton) {
+        let location = "Campsite"
+        if !checkboxsValue[1] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[1] = !checkboxsValue[1]
+    }
+    
+    @IBAction func pressedCheckbox_viewingpoint(_ sender: UIButton) {
+        let location = "ViewingPoint"
+        if !checkboxsValue[2] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[2] = !checkboxsValue[2]
+    }
+    
+    @IBAction func pressedCheckbox_busstop(_ sender: UIButton) {
+        let location = "BusStop"
+        if !checkboxsValue[3] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[3] = !checkboxsValue[3]
+    }
+    
+    @IBAction func pressedCheckbox_pier(_ sender: UIButton) {
+        let location = "Pier"
+        if !checkboxsValue[4] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[4] = !checkboxsValue[4]
+    }
+    
+    @IBAction func pressedCheckbox_beach(_ sender: UIButton) {
+        let location = "Beach"
+        if !checkboxsValue[5] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[5] = !checkboxsValue[5]
+    }
+    
+    @IBAction func pressedCheckbox_sittingoutarea(_ sender: UIButton) {
+        let location = "SittingOutArea"
+        if !checkboxsValue[6] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[6] = !checkboxsValue[6]
+    }
+    
+    @IBAction func pressedCheckbox_pavilion(_ sender: UIButton) {
+        let location = "Pavilion"
+        if !checkboxsValue[7] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[7] = !checkboxsValue[7]
+    }
+    
+    @IBAction func pressedCheckbox_sunrise(_ sender: UIButton) {
+        let location = "Sunrise"
+        if !checkboxsValue[8] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[8] = !checkboxsValue[8]
+    }
+    
+    @IBAction func pressedCheckbox_stargazing(_ sender: UIButton) {
+        let location = "Stargazing"
+        if !checkboxsValue[9] {
+            sender.setImage(UIImage(named: "Checkbox_checked"), for: .normal)
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = location
+            request.region = self.mapView.region
+            let search = MKLocalSearch(request: request)
+            search.start() {
+                (response, error) in
+                if error != nil {
+                    print("Error in search: \(error!.localizedDescription)")
+                } else if response!.mapItems.count > 0 {
+                    for item in response!.mapItems {
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = item.placemark.coordinate
+                        annotation.title = location
+                        self.mapView.addAnnotation(annotation)
+                    }
+                }
+            }
+        } else {
+            sender.setImage(UIImage(named: "Checkbox_unchecked"), for: .normal)
+            self.mapView.removeAnnotations(self.mapView.annotations.filter({$0.title == location}))
+        }
+        self.mapView.reloadInputViews()
+        checkboxsValue[9] = !checkboxsValue[9]
     }
 }
 
